12b3d5ccbbe450df727ba6ef8da17f33748d9512
FlorianDesjouis <desjouisflorian@gmail.com>
2016-01-31 23:19:27 +0100

    Exercice jeu de cartes

--- index.php
+++ index.php
@@ -9,14 +9,20 @@ if(isset($_POST['choice'])){
 require('card.php');
 //SCENARIO 1
 
-$deck = new Deck();
-$deck->shuffle();
-$bank = new Bank();
-$player = new Player($hand[]);
-$_SESSION['game_state'] = new GameState();
+if(empty($_SESSION['game_state'])){
+    $_SESSION['game_state'] = new GameState();
+    $deck = new Deck();
+    $deck->shuffle();
+    $bank = new Bank();
+    $player = new Player($hand[]);
+}else{
+    $deck = $_SESSION['game_state']->$deck;
+    $bank = $_SESSION['game_state']->$bank;
+    $player = $_SESSION['game_state']->$player;
+}
+
 $bank->take($deck->deal(2));
 $player->take($deck->deal(2));
-//tire 2 cartes du deck, la banque les prends
 
 while( $player->getHandValue() < 21){// || jusqu'à ce que le joueur arrête de tirer
     $player->take($deck->deal(1));
@@ -25,11 +31,14 @@ while( $player->getHandValue() < 21){// || jusqu'à ce que le joueur arrête de 
 if($player->getHandValue() > 21){
     echo "Le joueur perd ".$player->getHandValue();
     echo "La banque gagne";
+    unset($_SESSION['game_state']);
 }elseif($player->getHandValue() > $bank->getHandValue()){
     echo "Le joueur gagne";
+    unset($_SESSION['game_state']);
 }else{
     echo "Le joueur a ".$player->getHandValue();
     echo "Le joueur perd ".$player->getHandValue();
+    unset($_SESSION['game_state']);
 }
 
 while( $bank->getHandValue() < 17){
@@ -39,15 +48,19 @@ while( $bank->getHandValue() < 17){
 if($bank->getHandValue() > 21){
     echo "La banque perd ".$bank->getHandValue();
     echo "Le joueur gagne !";
+    unset($_SESSION['game_state']);
 }elseif($bank->getHandValue() > $player->getHandValue()){
     echo "La banque gagne";
+    unset($_SESSION['game_state']);
 }else{
     echo "La banque a ".$bank->getHandValue();
     echo "La banque perd ".$bank->getHandValue();
+    unset($_SESSION['game_state']);
 }
 
 if($player->getHandValue() == $bank->getHandValue()){
     echo "Egalité !";
+    unset($_SESSION['game_state']);
 }
 
 $serialized = serialize($_SESSION['game_state']);
